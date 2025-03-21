d6f630ea04c4fa2e901086f299f96ffbb1415b6a
Horri kalile <103006189+Horri-kalile@users.noreply.github.com>
2025-03-21 22:38:46 +1300

    Implemented GameRoom class to manage game rooms and players

---
+++ Server/GameRoom.py
@@ -0,0 +1,12 @@
+import uuid
+
+class GameRoom:
+    def _init_(self):
+        self.id = str(uuid.uuid4())  # Unique ID for the game room
+        self.player1 = {}  # Stores player 1's address and socket
+        self.player2 = {}  # Stores player 2's address and socket
+        self.gameStarted = False  # Tracks if the game has started
+
+    def _str_(self):
+        return f"Room ID: [ {self.id},\n Player 1: {self.player1},\n Player 2: {self.player2},\n Game Started: {self.gameStarted} \n ]"
+    
