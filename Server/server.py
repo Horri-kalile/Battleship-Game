5bdf5b790940349ec20c48a66cd3cffe771c6bdb
Horri kalile <103006189+Horri-kalile@users.noreply.github.com>
2025-03-25 18:56:40 -0600

    Implemented communication logic: handling player shots, results, and game end

--- Server/server.py
+++ Server/server.py
@@ -52,9 +52,75 @@ def main():
                             print("Room", found_room.id, "| Player that begins:", second_address)
                             server_socket.sendto((message + ";shoot").encode('utf-8'), second_address)
                             server_socket.sendto((message + ";wait").encode('utf-8'), first_address)
+            elif mess.lower().startswith("shoots"):
+
+                array = mess.lower().split(';')
+                sender_address = address
+                coords = array[0].replace(" ", "").removeprefix("shoots").replace("(", "").replace(")", "").split(",")
+
+                room, player_number = find_room(sender_address)
+                other_address = find_other_player_address(room, player_number)
+
+                if other_address:
+                    print("Room", room.id, "|Player", sender_address, "shoots on coordinates: ", coords)
+                    print("Room", room.id, "|Sending shoot verification to:", other_address)
+
+                    mes = "check" + str(coords) + ';' + room.id
+                    server_socket.sendto(mes.encode('utf-8'), other_address)
+
+            elif mess.lower().startswith("result"):
+                array = mess.split(';')
+                result = array[1]
+                sender_address = address
+                coords = array[5]
+
+                room, player_number = find_room(sender_address)
+                other_address = find_other_player_address(room, player_number)
+
+                if other_address:
+                    print("Room", room.id, "|Player", address, "verifies shoot as: ", result)
+                    print("Room", room.id, "|Updating boards", other_address)
+
+                    mes = "update;" + coords + ";" + str(result) + ";" + room.id
+                    server_socket.sendto(mes.encode('utf-8'), other_address)
+                if result == "False":
+                    server_socket.sendto("wait".encode('utf-8'), other_address)
+                    server_socket.sendto("shoot".encode('utf-8'), sender_address)
+                else:
+                    server_socket.sendto("wait".encode('utf-8'), sender_address)
+                    server_socket.sendto("shoot".encode('utf-8'), other_address)
+
+            elif mess.lower() == "end_you_won":
+
+                room, player_number = find_room(address)
+                other_address = find_other_player_address(room, player_number)
+
+                if other_address:
+                    print("Room", room.id, "|Player", other_address, "wins!")
+                    server_socket.sendto("winner".encode('utf-8'), other_address)
+                    remove_room_by_id(room.id)
+
+            elif mess.lower() == "end":
+                room, player_number = find_room(address)
+                other_address = find_other_player_address(room, player_number)
+                if other_address:
+                    server_socket.sendto("end".encode('utf-8'), other_address)
+
+                print(f"Room", room.id, "|Game has ended by Player {address}.")
+                remove_room_by_id(room.id)
+            else:
+                print("ERROR, Invalid message:",mess)
+                server_socket.sendto("ERROR, Invalid message".encode('utf-8'), address)
+            
+                           
         except socket.error as e:
             print(f"Socket error: {e}")
             exit()
+
+def remove_room_by_id(room_id):
+    global rooms
+    rooms = [room for room in rooms if room.id != room_id]
+    print(f"Game in room ${room_id} ended")
 
 def add_player_to_rooms(player):
     """Add a player to an available room or create a new room."""
@@ -87,6 +153,17 @@ def update_game_started(room_id, new_value):
             room.gameStarted = new_value
             break
 
+
+def find_other_player_address(room, player_number):
+    if room and player_number == 1:
+        return room.player2
+    elif room and player_number == 2:
+        return room.player1
+    else:
+        print("Error, room doesn't exist!")
+        return None
+    
+    
 def find_room(player):
     """Find the room where a player is located."""
     for room in rooms:
