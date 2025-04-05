76f8ff5a63e0afb972e18effe0cc943335e55fe5
Intissar99 <meddebintissar@gmail.com>
2025-04-05 18:13:20 +0200

    Implemented player shot input validation

--- Client/client_helpers.py
+++ Client/client_helpers.py
@@ -19,3 +19,34 @@ def is_ship_hit(board, x_coord, y_coord):
     if board[y_coord - 1][x_coord - 1] == "X":
         return True
     return False
+
+    
+
+def get_shot(shot_board):
+    try:
+        print("Enter your shot")
+        x_coord = input("Enter x coordinate: ")
+
+        if x_coord == "end":
+            return -1, -1, True
+
+        y_coord = input("Enter y coordinate: ")
+
+        if y_coord == "end":
+            return -1, -1, True
+
+        x_coord = int(x_coord)
+        y_coord = int(y_coord)
+
+        if 1 <= x_coord <= 10 and 1 <= y_coord <= 10:
+            if shot_board[y_coord-1][x_coord-1] == "-":
+                return x_coord, y_coord, False
+            else:
+                print("This place has already been shot! Enter new data.")
+                return get_shot(shot_board)
+
+        print("Enter a value in the range of 1-10!")
+        return get_shot(shot_board)
+    except ValueError:
+        print("Enter a numerical value in the range of 1-10!")
+        return get_shot(shot_board)
