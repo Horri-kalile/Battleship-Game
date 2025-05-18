1c97be62ed47578a2bd5960fee0a80a5c7d486af
Horri kalile <103006189+Horri-kalile@users.noreply.github.com>
2025-05-18 00:18:38 -0600

    fix constructor and string method in GameRoom

--- Server/GameRoom.py
+++ Server/GameRoom.py
@@ -1,12 +1,12 @@
 import uuid
 
 class GameRoom:
-    def _init_(self):
+    def __init__(self):
         self.id = str(uuid.uuid4())  # Unique ID for the game room
         self.player1 = {}  # Stores player 1's address and socket
         self.player2 = {}  # Stores player 2's address and socket
         self.gameStarted = False  # Tracks if the game has started
 
-    def _str_(self):
+    def __str__(self):
         return f"Room ID: [ {self.id},\n Player 1: {self.player1},\n Player 2: {self.player2},\n Game Started: {self.gameStarted} \n ]"
     
